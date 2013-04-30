06599746c53a1859e2f6c24d5ce006df22703779
Francisco Facioni <fran6co@gmail.com>
2013-04-29 21:34:27 -0300

    update to the latest version de opencv 2.4.5

--- opencv.rb
+++ opencv.rb
@@ -1,17 +1,9 @@
 require 'formula'
 
-def which_python
-  "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
-end
-
-def site_package_dir
-  "lib/#{which_python}/site-packages"
-end
-
 class Opencv < Formula
   homepage 'http://opencv.org/'
-  url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.4/OpenCV-2.4.4a.tar.bz2'
-  sha1 '6e518c0274a8392c0c98d18ef0ef754b9c596aca'
+  url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz'
+  sha1 '9e25f821db9e25aa454a31976ba6b5a3a50b6fa4'
 
   env :std # to find python
 
@@ -19,7 +11,6 @@ class Opencv < Formula
   option 'with-qt',  'Build the Qt4 backend to HighGUI'
   option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
   option 'with-opencl', 'Enable gpu code in OpenCV using OpenCL'
-  option 'with-ffmpeg', 'Enable ffmpeg video input'
   option 'with-openni', 'Enable support for OpenNI.'
 
   depends_on 'cmake' => :build
@@ -31,18 +22,12 @@ class Opencv < Formula
   depends_on 'jasper'  => :optional
   depends_on 'tbb'     => :optional
   depends_on 'qt'      => :optional
-  depends_on 'ffmpeg'  => :optional
-  depends_on 'openexr' => :optional
   depends_on 'totakke/openni/openni' if build.with? 'openni'
   depends_on :libpng
 
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
   # in Homebrew anyway. Will depend on openexr if it's installed.
-
-  # Fix non-ASCII characters breaking in Java 1.7
-  # https://github.com/Itseez/opencv/pull/718
-  def patches; DATA; end
 
   def install
     args = std_cmake_args + %w[
@@ -53,10 +38,8 @@ class Opencv < Formula
       -DBUILD_PNG=OFF
       -DBUILD_JPEG=OFF
       -DBUILD_JASPER=OFF
-      -DBUILD_OPENEXR=OFF
       -DBUILD_TESTS=OFF
       -DBUILD_PERF_TESTS=OFF
-      -DENABLE_PRECOMPILED_HEADERS=ON
     ]
     if build.build_32_bit?
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
@@ -66,16 +49,7 @@ class Opencv < Formula
     args << '-DWITH_QT=ON' if build.with? 'qt'
     args << '-DWITH_TBB=ON' if build.with? 'tbb'
     args << '-DWITH_OPENCL=ON' if build.with? 'opencl'
-    args << '-DWITH_FFMPEG=ON' if build.with? 'ffmpeg'
     args << '-DWITH_OPENNI=ON' if build.with? 'openni'
-
-    # Check for SIMD code support
-    if ENV.compiler == :clang
-      # not sure how to check SSSE3 hardware support but this should be safe
-      args << '-DENABLE_SSSE3=ON' << '-DENABLE_SSE41=ON' if (Hardware::CPU.family == :penryn or Hardware::CPU.sse4?) and ENV.compiler == :clang
-      args << '-DENABLE_SSE42=ON' if Hardware::CPU.sse4? and ENV.compiler == :clang
-      args << '-DENABLE_AVX=ON' if Hardware::CPU.avx? and ENV.compiler == :clang
-    end
 
     # The CMake `FindPythonLibs` Module is dumber than a bag of hammers when
     # more than one python installation is available---for example, it clings
@@ -115,6 +89,14 @@ class Opencv < Formula
     end
   end
 
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+  end
+
+  def site_package_dir
+    "lib/#{which_python}/site-packages"
+  end
+
   def caveats; <<-EOS.undent
     The OpenCV Python module will not work until you edit your PYTHONPATH like so:
       export PYTHONPATH="#{HOMEBREW_PREFIX}/#{site_package_dir}:$PYTHONPATH"
@@ -141,19 +123,3 @@ index 7541868..f1455e8 100644
  endif()
 
  if(OPENNI_LIBRARY AND OPENNI_INCLUDES)
-diff --git a/modules/java/build.xml.in b/modules/java/build.xml.in
-index 98ba2e3..c1c1854 100644
---- a/modules/java/build.xml.in
-+++ b/modules/java/build.xml.in
-@@ -8,8 +8,9 @@
-     <!-- http://stackoverflow.com/questions/3584968/ant-how-to-compile-jar-that-includes-source-attachment -->
-     <javac sourcepath="" srcdir="src" destdir="src" debug="on" includeantruntime="false" >
-       <include name="**/*.java"/>
-+      <compilerarg line="-encoding utf-8"/>
-     </javac>
- 
-     <jar basedir="src" destfile="bin/@JAR_NAME@"/>
-   </target>
--</project>
-\ No newline at end of file
-+</project>
