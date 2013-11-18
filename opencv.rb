ffa2180d97b6a030a97bcd5cced9b7e320fe0262
Francisco Facioni <fran6co@gmail.com>
2013-11-18 12:31:19 -0300

    Upstream change

--- opencv.rb
+++ opencv.rb
@@ -2,20 +2,19 @@ require 'formula'
 
 class Opencv < Formula
   homepage 'http://opencv.org/'
-  url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz'
-  sha1 '9e25f821db9e25aa454a31976ba6b5a3a50b6fa4'
-
-  env :std # to find python
+  url 'http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.6.1/opencv-2.4.6.1.tar.gz'
+  sha1 'e015bd67218844b38daf3cea8aab505b592a66c0'
 
   option '32-bit'
   option 'with-qt',  'Build the Qt4 backend to HighGUI'
   option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
-  option 'with-opencl', 'Enable gpu code in OpenCV using OpenCL'
+  option 'without-opencl', 'Disable gpu code in OpenCV using OpenCL'
   option 'with-openni', 'Enable support for OpenNI.'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
   depends_on 'numpy' => :python
+  depends_on :python
 
   depends_on 'eigen'   => :optional
   depends_on 'libtiff' => :optional
@@ -28,9 +27,16 @@ class Opencv < Formula
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
   # in Homebrew anyway. Will depend on openexr if it's installed.
+  depends_on 'ffmpeg' => :optional
+
+  def patches
+    [
+       DATA
+    ]
+  end
 
   def install
-    args = std_cmake_args + %w[
+    args = std_cmake_args + %W[
       -DCMAKE_OSX_DEPLOYMENT_TARGET=
       -DWITH_CUDA=OFF
       -DBUILD_ZLIB=OFF
@@ -40,7 +46,11 @@ class Opencv < Formula
       -DBUILD_JASPER=OFF
       -DBUILD_TESTS=OFF
       -DBUILD_PERF_TESTS=OFF
+      -DPYTHON_INCLUDE_DIR='#{python.incdir}'
+      -DPYTHON_LIBRARY='#{python.libdir}/lib#{python.xy}.dylib'
+      -DPYTHON_EXECUTABLE='#{python.binary}'
     ]
+
     if build.build_32_bit?
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
       args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
@@ -48,38 +58,10 @@ class Opencv < Formula
     end
     args << '-DWITH_QT=ON' if build.with? 'qt'
     args << '-DWITH_TBB=ON' if build.with? 'tbb'
-    args << '-DWITH_OPENCL=ON' if build.with? 'opencl'
     args << '-DWITH_OPENNI=ON' if build.with? 'openni'
-
-    # The CMake `FindPythonLibs` Module is dumber than a bag of hammers when
-    # more than one python installation is available---for example, it clings
-    # to the Header folder of the system Python Framework like a drowning
-    # sailor.
-    #
-    # This code was cribbed from the VTK formula and uses the output to
-    # `python-config` to do the job FindPythonLibs should be doing in the first
-    # place.
-    python_prefix = `python-config --prefix`.strip
-    # Python is actually a library. The libpythonX.Y.dylib points to this lib, too.
-    if File.exist? "#{python_prefix}/Python"
-      # Python was compiled with --framework:
-      args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-      if !MacOS::CLT.installed? and python_prefix.start_with? '/System/Library'
-        # For Xcode-only systems, the headers of system's python are inside of Xcode
-        args << "-DPYTHON_INCLUDE_DIR='#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/2.7/Headers'"
-      else
-        args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/Headers'"
-      end
-    else
-      python_lib = "#{python_prefix}/lib/lib#{which_python}"
-      if File.exists? "#{python_lib}.a"
-        args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
-      else
-        args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
-      end
-      args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/include/#{which_python}'"
-    end
-    args << "-DPYTHON_PACKAGES_PATH='#{lib}/#{which_python}/site-packages'"
+    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
+    args << '-DWITH_OPENCL=OFF' if build.without? 'opencl' or MacOS.version < :lion
+    args << '-DWITH_FFMPEG=OFF' unless build.with? 'ffmpeg'
 
     args << '..'
     mkdir 'macbuild' do
@@ -89,23 +71,11 @@ class Opencv < Formula
     end
   end
 
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
-  end
 
-  def site_package_dir
-    "lib/#{which_python}/site-packages"
-  end
-
-  def caveats; <<-EOS.undent
-    The OpenCV Python module will not work until you edit your PYTHONPATH like so:
-      export PYTHONPATH="#{HOMEBREW_PREFIX}/#{site_package_dir}:$PYTHONPATH"
-
-    To make this permanent, put it in your shell's profile (e.g. ~/.profile).
-    EOS
+  def caveats
+    python.standard_caveats if python
   end
 end
-
 # If openni was installed using homebrew, look for it on the proper path
 __END__
 diff --git a/cmake/OpenCVFindOpenNI.cmake b/cmake/OpenCVFindOpenNI.cmake
@@ -118,8 +88,8 @@ index 7541868..f1455e8 100644
  elseif(UNIX OR APPLE)
 -    find_file(OPENNI_INCLUDES "XnCppWrapper.h" PATHS "/usr/include/ni" "/usr/include/openni" DOC "OpenNI c++ interface header")
 -    find_library(OPENNI_LIBRARY "OpenNI" PATHS "/usr/lib" DOC "OpenNI library")
-+    find_file(OPENNI_INCLUDES "XnCppWrapper.h" PATHS "#{HOMEBREW_PREFIX}/include/ni" "/usr/include/ni" "/usr/include/openni" DOC "OpenNI c++ interface header")
-+    find_library(OPENNI_LIBRARY "OpenNI" PATHS "#{HOMEBREW_PREFIX}/lib" "/usr/lib" DOC "OpenNI library")
++    find_file(OPENNI_INCLUDES "XnCppWrapper.h" PATHS "HOMEBREW_PREFIX/include/ni" "/usr/include/ni" "/usr/include/openni" DOC "OpenNI c++ interface header")
++    find_library(OPENNI_LIBRARY "OpenNI" PATHS "HOMEBREW_PREFIX/lib" "/usr/lib" DOC "OpenNI library")
  endif()
 
  if(OPENNI_LIBRARY AND OPENNI_INCLUDES)
